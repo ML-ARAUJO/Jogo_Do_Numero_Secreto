7c2b94fb4db87ec77657f83fb0778b5a30c3a1d8
Matheus Araujo <matheuslp.daraujo@gmail.com>
2025-09-23 21:14:07 -0400

    Update app.js
    comentando a condição if else do voice, para aplicar o responsiveVoice.speak

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     //Narrar o texto
-    //responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
-    if ('speechSynthesis' in window) {
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    /*if ('speechSynthesis' in window) {
         let utterance = new SpeechSynthesisUtterance(texto);
         utterance.lang = 'pt-BR'; 
         utterance.rate = 1.2; 
         window.speechSynthesis.speak(utterance); 
     } else {
         console.log("Web Speech API não suportada neste navegador.");
-    }
+    }*/
 }
 
 function exibirMensagemInicial() {
